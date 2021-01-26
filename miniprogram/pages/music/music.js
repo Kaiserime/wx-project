00f0928d7f47b69685eb5f0e03b699556ee811ef
zyh <877394414@qq.com>
2021-01-26 22:08:03 +0800

    first

--- miniprogram/pages/music/music.js
+++ miniprogram/pages/music/music.js
@@ -20,7 +20,57 @@ Page({
 },
 {url:'http://p1.music.126.net/pOXTFta-mhTpZOGhBBWvhQ==/109951165664682857.jpg?imageView&quality=89'
 },
-  ]
+  ],
+  playlist:[
+    {
+    "id":"1001",
+    "playCount":"14451615",
+    "name":"你一定要在自己热爱的世界里闪闪发亮啊",
+    "picUrl":"https://p1.music.126.net/uesfHcJmZ23S3er_1mpeaw==/109951165621856219.jpg?param=140y140"
+  }, 
+   {
+    "id":"1002",
+    "playCount":"280155214",
+    "name":"我试着把孤独藏进耳机",
+    "picUrl":"https://p1.music.126.net/Xvo6PwBcdOA69ipcpV9YYg==/109951165463253777.jpg?param=140y140"
+  },
+  {
+    "id":"1003",
+    "playCount":"3325158",
+    "name":"【翻/原】温柔不是我说 而是你觉得.",
+    "picUrl":"https://p1.music.126.net/PJylNWy_2-jI7LRgQ2Cm6w==/109951165649129522.jpg?param=140y140"
+  },
+  {
+    "id":"1004",
+    "playCount":"320155",
+    "name":"2021·心里装着鲜花银河星光和我爱的人",
+    "picUrl":"https://p2.music.126.net/O8LkkfC7PtV7TA4UP693XA==/109951164569667332.jpg?param=140y140"
+  },
+  {
+    "id":"1005",
+    "playCount":"31201111",
+    "name":"宝藏雷达 | 惊喜总出现于不经意间",
+    "picUrl":"https://p4.music.126.net/Hy4B4dD5jRh3NSyennIymA==/109951165559549105.jpg?param=140y140"
+  },
+  {
+    "id":"1006",
+    "playCount":"3121265",
+    "name":"神秘雷达 | 来自神秘歌友的推荐",
+    "picUrl":" https://p4.music.126.net/LB6hcyND-KTqBlfYMkbEnw==/109951165480872644.jpg?param=140y140"
+  },
+  {
+    "id":"1007",
+    "playCount":"120215145",
+    "name":"韩甜 | 轻快旋律·山风不及你温柔",
+    "picUrl":"https://p3.music.126.net/UpyW_JOHIP2mNCGSYtyUhA==/109951165131295355.jpg?param=140y140"
+  },
+  {
+    "id":"1008",
+    "playCount":"3905115",
+    "name":"18年的那个夏天除了有点热什么都好",
+    "picUrl":"https://p3.music.126.net/TxUiutliWpGME0x_sm6ldw==/109951164988260937.jpg?param=140y140"
+  },
+]
 
   },
 
